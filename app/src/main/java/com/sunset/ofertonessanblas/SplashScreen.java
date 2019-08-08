52701c18bdf6515f21029173a6e59860eb9ad834
Cesar Espino <danger_95@hotmail.es>
2019-08-08 10:26:47 -0600

    Initial commit

---
+++ app/src/main/java/com/sunset/ofertonessanblas/SplashScreen.java
@@ -0,0 +1,20 @@
+package com.sunset.ofertonessanblas;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+
+    }
+}
